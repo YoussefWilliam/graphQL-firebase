68926d96d8d98fa669384e93e654636004309682
Youssef William <youssef.william997@gmail.com>
2021-03-03 23:48:37 +0200

    Creating first mutation schema

--- src/resolvers.js
+++ src/resolvers.js
@@ -1,28 +1,40 @@
 const fetch = require("node-fetch");
+const firebaseClient = require("./firebase");
 
 const url = "https://graphql-34f89-default-rtdb.firebaseio.com/";
+
+const usersDatabase = firebaseClient.database();
 
 const resolvers = {
   Query: {
     users: async () => {
-      const data = await fetch(
-        `${url}/users.json`
-      );
+      const data = await fetch(`${url}/users.json`);
       const dataJson = await data.json();
       const keys = Object.keys(dataJson);
-      const arrayOfUserData = keys.map(user => {
+      const arrayOfUserData = keys.map((user) => {
         let myUserData = dataJson[user];
         let myUserGraphQL = {
           fullName: myUserData.fullName,
           home: myUserData.home,
           age: myUserData.age,
           uni: myUserData.uni,
-        }
-        
+        };
+
         return myUserGraphQL;
-      })
-      console.log("data:::", arrayOfUserData)
-      return arrayOfUserData
+      });
+      return arrayOfUserData;
+    },
+  },
+  Mutation: {
+    addUser: async (root, { fullName, age, home, uni }, { api }, info) => {
+      const newUser = {
+        fullName: fullName,
+        home: home,
+        age: age,
+        uni: uni,
+      };
+      await usersDatabase.ref("users/").push(newUser);
+      return true;
     },
   },
 };
